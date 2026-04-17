e57701bfaf56b6c938e56c6dbf9b7f390f7b0f48
Drapliks <kinstevg@gmail.com>
2026-04-17 14:37:46 +0500

    update installer.py

--- installer.py
+++ installer.py
@@ -11,12 +11,11 @@ os.system('clear')
 print("Welcome to Drapliks Config for Niri(DC4N) installer!\n1.Minimal install\n2.Custom install\n3.Exit")
 chs = input("> ")
 
-while True:
-    if chs == "1":
+if chs == "1":
         os.system("bash components/minimalinstall.sh")
         sys.exit()
-    if chs == "2":
+elif chs == "2":
         custominstall.installC()
         sys.exit()
-    if chs == "3":
+elif chs == "3":
         sys.exit()
