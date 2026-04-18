09ecc9d8a6db444f6acd07807adb4f46be79e2f1
Drapliks <kinstevg@gmail.com>
2026-04-18 15:51:50 +0500

    update

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ bash minimalinstall.sh
 # Packages
 Base packages:
 ```
-pulseaudio fish pipewire xdg-desktop-portal-gnome papirus-icon-theme btop mpv wine pipewire-pulse flatpak pipewire-alsa pipewire-jack wireplumber swww ttf-jetbrains-mono-nerd viewnior ttf-font-awesome otf-font-awesome gnome-themes-extra niri-settings-git alacritty mako swaybg swaylock waybar xwayland-satellite thunar nwg-look xdg-desktop-portal-gtk zen-browser-bin pipes.sh yay
+pulseaudio fish pipewire xdg-desktop-portal-gnome papirus-icon-theme btop mpv wine pipewire-pulse flatpak pipewire-alsa pipewire-jack wireplumber swww ttf-jetbrains-mono-nerd viewnior ttf-font-awesome otf-font-awesome gnome-themes-extra niri-settings-git alacritty mako swaybg swaylock waybar xwayland-satellite nwg-look xdg-desktop-portal-gtk zen-browser-bin pipes.sh yay
 ```
 Dev packages:
 ```
--- components/minimalinstall.sh
+++ components/minimalinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yes | sudo pacman -Syy
-yes | sudo pacman -S pulseaudio fish pipewire xdg-desktop-portal-gnome papirus-icon-theme btop mpv wine pipewire-pulse flatpak pipewire-alsa pipewire-jack wireplumber swww ttf-jetbrains-mono-nerd viewnior ttf-font-awesome otf-font-awesome gnome-themes-extra niri-settings-git alacritty mako swaybg swaylock waybar xwayland-satellite thunar nwg-look xdg-desktop-portal-gtk
+yes | sudo pacman -S pulseaudio fish pipewire xdg-desktop-portal-gnome papirus-icon-theme btop mpv wine pipewire-pulse flatpak pipewire-alsa pipewire-jack wireplumber swww ttf-jetbrains-mono-nerd viewnior ttf-font-awesome otf-font-awesome gnome-themes-extra niri-settings-git alacritty mako swaybg swaylock waybar xwayland-satellite nwg-look xdg-desktop-portal-gtk
 cd ~/
 git clone https://aur.archlinux.org/packages/yay
 cd yay
@@ -10,7 +10,7 @@ yay -S zen-browser-bin pipes.sh
 cd ~/DC4N
 cp -r wallpapers/ ~/
 cp -r .config/ ~/
-chsh -s $(which fish)
 gsettings set org.gnome.desktop.interface accent-color 'slate'
+chsh -s $(which fish)
 clear
 echo "Minimal install is end."
